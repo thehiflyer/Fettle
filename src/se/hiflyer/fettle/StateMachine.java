cb736c4c7fbfc098840c3f86b58b7acff16b09fc
thehiflyer <per.malmen@gmail.com>
2011-02-10 19:18:20 +0100

    Added some javadoc

--- src/se/hiflyer/fettle/StateMachine.java
+++ src/se/hiflyer/fettle/StateMachine.java
@@ -1,11 +1,41 @@
 package se.hiflyer.fettle;
 
+/**
+ * A view of a state machine from a users perspective, i.e it is immutable and already set up.
+ *
+ * @param <S> the type of the states
+ * @param <E> the type of the events that can trigger state changes
+ */
 public interface StateMachine<S, E> {
+	/**
+	 * Gets the current state of the machine
+	 *
+	 * @return the state the machine is currently in
+	 */
 	S getCurrentState();
 
+	/**
+	 * Fires an event at the state machine, possibly triggering a state change
+	 *
+	 * @param event the event that is fired
+	 * @return true if the event resulted in a state change, false otherwise
+	 */
 	boolean fireEvent(E event);
 
+	/**
+	 * Fires an event at the state machine, possibly triggering a state change
+	 *
+	 * @param event the event that is fired
+	 * @param args  the arguments to be sent to any actions that are run on a state change
+	 * @return true if the event resulted in a state change, false otherwise
+	 */
 	boolean fireEvent(E event, Arguments args);
 
+	/**
+	 * Forces the state machine to enter the forcedState even if there are no transitions leading to it
+	 * No transition actions are run but exit actions on the current state and entry actions on the new state are run
+	 *
+	 * @param forcedState the state the machine will be in after this method is called
+	 */
 	void forceSetState(S forcedState);
 }
