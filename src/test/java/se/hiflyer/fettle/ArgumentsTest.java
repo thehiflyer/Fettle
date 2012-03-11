2dea6ef4da241773426d3b99238fc19572c646ee
hiflyer <per.malmen@gmail.com>
2012-03-12 00:05:11 +0100

    Added a few more arguments tests

--- src/test/java/se/hiflyer/fettle/ArgumentsTest.java
+++ src/test/java/se/hiflyer/fettle/ArgumentsTest.java
@@ -3,12 +3,13 @@ package se.hiflyer.fettle;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class ArgumentsTest {
 
 	@Test
-	public void testGetFirst() throws Exception {
+	public void getFirst() throws Exception {
 		Arguments args = new Arguments("first");
 		assertEquals(1, args.getNumberOfArguments());
 		assertEquals("first", args.getFirst());
@@ -17,7 +18,7 @@ public class ArgumentsTest {
 	}
 
 	@Test
-	public void testMoreArguments() throws Exception {
+	public void moreArguments() throws Exception {
 		Arguments args = new Arguments("first", "second", "third");
 		assertEquals(3, args.getNumberOfArguments());
 		assertEquals("first", args.getFirst());
@@ -27,9 +28,32 @@ public class ArgumentsTest {
 	}
 
 	@Test
-	public void testOutOfBounds() throws Exception {
+	public void outOfBounds() throws Exception {
 		Arguments args = new Arguments("first", "second", "third");
 		assertTrue(null == args.getArgument(-1));
 		assertTrue(null == args.getArgument(3));
 	}
+
+    @Test
+    public void toStringIncludesAllListed() throws Exception {
+        Arguments args = new Arguments("first", "second", "third");
+        String toString = args.toString();
+        assertTrue(toString.contains("first"));
+        assertTrue(toString.contains("second"));
+        assertTrue(toString.contains("third"));
+    }
+
+    @Test
+    public void toStringWithOnlyOneArg() throws Exception {
+        Arguments args = new Arguments("foo");
+        String toString = args.toString();
+        assertTrue(toString.contains("foo"));
+    }
+
+    @Test
+    public void toStringNoArgs() throws Exception {
+        Arguments noArgs = Arguments.NO_ARGS;
+        String toString = noArgs.toString();
+        assertFalse(toString.isEmpty());
+    }
 }
