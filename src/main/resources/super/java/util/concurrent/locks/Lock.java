18b0087964d7c5381c5a1d6bea2335997f5115c7
Dema <magnus2468@gmail.com>
2012-09-06 12:03:15 +0200

    Intellij and gwt fix

---
+++ src/main/resources/super/java/util/concurrent/locks/Lock.java
@@ -0,0 +1,6 @@
+package java.util.concurrent.locks;
+
+public interface Lock {
+    void lock();
+    void unlock();
+}
---
+++ src/main/resources/super/java/util/concurrent/locks/ReentrantLock.java
@@ -0,0 +1,13 @@
+package java.util.concurrent.locks;
+
+
+public class ReentrantLock implements Lock {
+
+    @Override
+    public void lock() {
+    }
+
+    @Override
+    public void unlock() {
+    }
+}
