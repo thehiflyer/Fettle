949247406126bed90bd32f3c808149b19cb01a48
hiflyer <per.malmen@gmail.com>
2015-02-04 16:28:27 +0100

    Added failing tests for internal transitions

---
+++ src/test/java/se/fearless/fettle/InternalTransitionsTest.java
@@ -0,0 +1,56 @@
+package se.fearless.fettle;
+
+import org.junit.Before;
+import org.junit.Test;
+import se.fearless.fettle.builder.StateMachineBuilder;
+import se.mockachino.annotations.Mock;
+
+import static se.mockachino.Mockachino.*;
+import static se.mockachino.matchers.Matchers.any;
+
+public class InternalTransitionsTest {
+
+	public static final String SELF_TRANSITION_TRIGGER = "self";
+	@Mock
+	private Action<States, String, Void> transitionAction;
+
+	@Mock
+	private Action<States, String, Void> entryAction;
+
+	@Mock
+	private Action<States, String, Void> exitAction;
+
+	private StateMachine<States, String, Void> stateMachine;
+
+	@Before
+	public void setUp() throws Exception {
+		setupMocks(this);
+		StateMachineBuilder<States, String, Void> builder = Fettle.newBuilder(States.class, String.class);
+		builder.transition().internal(States.INITIAL).on(SELF_TRANSITION_TRIGGER).perform(transitionAction);
+		builder.onEntry(States.INITIAL).perform(entryAction);
+		builder.onExit(States.INITIAL).perform(exitAction);
+		stateMachine = builder.build(States.INITIAL);
+
+		stateMachine.fireEvent(SELF_TRANSITION_TRIGGER);
+	}
+
+	@Test
+	public void internalTransitionPerformsTransitionActions() throws Exception {
+		verifyActionInvocation(transitionAction);
+	}
+
+	@Test
+	public void internalTransitionDoesNotPerformEntryActions() throws Exception {
+		verifyNever().on(entryAction).onTransition(States.INITIAL, States.INITIAL, SELF_TRANSITION_TRIGGER, any(Void.class), stateMachine);
+	}
+
+	@Test
+	public void internalTransitionPerformsExitActions() throws Exception {
+		verifyNever().on(exitAction).onTransition(States.INITIAL, States.INITIAL, SELF_TRANSITION_TRIGGER, any(Void.class), stateMachine);
+	}
+
+	private void verifyActionInvocation(Action<States, String, Void> action) {
+		verifyOnce().on(action).onTransition(States.INITIAL, States.INITIAL, SELF_TRANSITION_TRIGGER, any(Void.class), stateMachine);
+	}
+
+}
