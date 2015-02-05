b068d76ad9a12f2c9de587b297bd445e017fc096
hiflyer <per.malmen@gmail.com>
2015-02-05 11:56:37 +0100

    Added an abstract base class for all transitions

--- src/main/java/se/fearless/fettle/impl/BasicTransition.java
+++ src/main/java/se/fearless/fettle/impl/BasicTransition.java
@@ -2,41 +2,12 @@ package se.fearless.fettle.impl;
 
 import se.fearless.fettle.Action;
 import se.fearless.fettle.Condition;
-import se.fearless.fettle.StateMachine;
-import se.fearless.fettle.Transition;
-import se.fearless.fettle.util.GuavaReplacement;
 
 import java.util.Collection;
 
-public class BasicTransition<S, E, C> implements Transition<S, E, C> {
-	private final S to;
-	private final Condition<C> condition;
-	private final Collection<Action<S, E, C>> actions = GuavaReplacement.newArrayList();
+public class BasicTransition<S, E, C> extends AbstractTransition<S,E,C> {
 
 	public BasicTransition(S to, Condition<C> condition, Collection<Action<S, E, C>> actions) {
-		this.to = to;
-		this.condition = condition;
-		this.actions.addAll(actions);
-	}
-
-	@Override
-	public S getTo() {
-		return to;
-	}
-
-	@Override
-	public boolean isSatisfied(C context) {
-		return condition.isSatisfied(context);
-	}
-
-	public Condition<C> getCondition() {
-		return condition;
-	}
-
-	@Override
-	public void onTransition(S from, S to, E event, C context, StateMachine<S, E, C> statemachine) {
-		for (Action<S, E, C> action : actions) {
-			action.onTransition(from, to, event, context, statemachine);
-		}
+		super(condition, to, actions);
 	}
 }
